6532c45cb96e867ac5e06a3b8e9271989f635151
Geometry Tryhard <memelite69@gmail.com>
2025-02-12 20:11:01 -0500

    update

--- assets/js/load_game.js
+++ assets/js/load_game.js
@@ -1,15 +1,14 @@
-function loadgame(gameURL){
+function loadgame(gameURL) {
   const element = document.getElementById("gameframe_frame");
   if (element !== null) {
     element.src = gameURL;
-    window.
-    localStorage.setItem('gameURL', element.src);
+    window.localStorage.setItem('gameURL', element.src);
     element.contentWindow.focus();
     element.scrollIntoView({ 
       behavior: "smooth" 
     }); 
-  }
-  else{
+
+     } else {
     localStorage.setItem("gameURL", gameURL);
 
     const text = gameURL;
@@ -23,25 +22,6 @@ function loadgame(gameURL){
       console.log("Snippet not found.");
       game2 = gameURL;
     }
-    window.location.href = "gameframe.html#" + game2;
+    window.location.href = "gameframe.html";
   }
-
-
 }
-const gameFrames = document.querySelectorAll('.gameframe');
-
-gameFrames.forEach(gameFrame => {
-  gameFrame.addEventListener('click', () => {
-    // Find the h1 within the clicked gameFrame
-    const h1 = gameFrame.querySelector('h1');
-
-    // Get the text content of the h1
-    const gameName = h1.textContent.trim();
-
-    // Now you have the game name
-    console.log("Game Name:", gameName);
-
-    // You can use gameName for your loadgame function or other actions
-    // loadgame(gameName); // Example
-  });
-});
